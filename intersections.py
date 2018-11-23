211d3933c6b4b7e4164e70b666a529e367695ff7
Carolyn Lee <cnl2@andrew.cmu.edu>
2018-11-23 12:30:18 -0800

    wrote intersection class but haven't tested it yet oyyyyy

--- intersections.py
+++ intersections.py
@@ -1,11 +1,11 @@
 from simulation import *
-
+from roads import *
         # has a list of the roads that run thru it and can control what cars go thru for each intersection. just makes all the parallel ones go simultaneously. if it is not 4way then teh roads can handle moving the cars right or left. otherwise, the cars are only going to go straight.
         
-        # there are only 1 or 2 cars in roadsNS or roads EW....
+        # there are only 1 or 2 roads in roadsNS or roads EW....
         
-        # needs to handle the cars that are going through the intersection
-class Intersection (object):
+        # needs to handle the cars that are going through the intersection: picks them up as they enter intersection and then drops them off as they go into next road.
+class Intersection (Road):
     def __init__(self, data, roadsNS, roadsEW, x, y, NSTime, EWTime, staggerTime):
         self.roadsNS = roadsNS
         self.roadsEW = roadsEW
@@ -17,10 +17,18 @@ class Intersection (object):
         #sometimes you don't want to start with NS being green. sometimes, want
         #to stagger the times 
         self.staggerTime = staggerTime
+        self.carsEW = []
+        self.carsWE = []
+        self.carsNS = []
+        self.carsSN = []
     
     def changeLights (roadsList, light):
         for road in roadsList:
-            road.Light = light
+            #need to check which side of the road is in this intersection
+            if road[1] == "P":
+                road.lightP = light
+            elif road [1] == "N":
+                road.lightN = light
     
     def checkLights (self, data):
         if timerIsNSecs (data, self.cycle, self.staggerTime):
@@ -37,10 +45,90 @@ class Intersection (object):
                             self.NSTime + self.staggerTime):
             changeLights (roadsNS, 0)
             changeLights (roadsEW, 2)
-    #handle cars coming thru the intersection: 
+    #handle cars coming thru the intersection:
+        #takes cars that are coming into the intersection so that it can deal w/
+    def pickUpCars (self, data):
+        for road in roadsNS:
+            if road [1] == "P":
+                if road.lightP == 1:
+                    tmpCar = road.carOutP(data)
+                    if tmpCar != None:
+                        carsNS += tmpCar
+                        road.carsListP.remove (tmpCar)
+            elif road [1] == "N":
+                if road.lightN == 1:
+                    tmpCar = road.carOutN(data)
+                    if tmpCar != None:
+                        carsSN += tmpCar
+                        road.carsListN.remove (tmpCar)
+        for road in roadsEW:
+            if road [1] == "P":
+                if road.lightP == 1:
+                    tmpCar = road.carOutP(data)
+                    if tmpCar != None:
+                        carsWE += tmpCar
+                        road.carsListP.remove (tmpCar)
+            elif road [1] == "N":
+                if road.lightN == 1:
+                    tmpCar = road.carOutN(data)
+                    if tmpCar != None:
+                        carsEW += tmpCar
+                        road.carsListN.remove (tmpCar)
+    def dropOffCars (self, data):
+        for road in roadsNS:
+            if road[1] =="N":
+                for car in self.carsNS:
+                    #if out of intersection, get rid of this car and create a new car 
+                    #to stick in the other road
+                    if car.y > data.intersecRad + self.y:
+                        road[0].carInN(data, curSpeed = car.curSpeed)
+                        self.carsNS.remove(car)
+            if road [1] == "P":
+                for car in self.carsSN:
+                    if car.y < -data.intersecRad + self.y:
+                        road[0].carInP(data, curSpeed = car.curSpeed)
+                        self.carsNS.remove(car)
+        for road in roadsEW:
+            if road[1] =="N":
+                for car in self.carsWE:
+                    if car.x > data.intersecRad + self.x:
+                        road[0].carInN(data, curSpeed = car.curSpeed)
+                        self.carsWE.remove(car)
+            if road [1] == "P":
+                for car in self.carsEW:
+                    if car.x < -data.intersecRad + self.x:
+                        road[0].carInP(data, curSpeed = car.curSpeed)
+                        self.carsEW.remove(car)
+                
+
+    def changeAccelCarsH (self, carsList):
+        for c in range(1, len(carsList)):
+            car1 = carsList [c]
+            car2 = carsList [c-1]
+            if self.isTooClose (car1, car2):
+                car1.deceler()
+            else:
+                self.car1.acceler()
+        if carsList != []:
+            carsList[0].acceler()
+    def changeAccelAllCars (self):
+        self.changeAccelCarsH (self.carsEW)
+        self.changeAccelCarsH (self.carsWE)
+        self.changeAccelCarsH (self.carsNS)
+        self.changeAccelCarsH (self.carsSN)
+    
+            
     def timerFiredIntersec (self, data):
+        self.pickUpCars (data)
+        self.moveCars()
+        self.changeAccelAllCars()
+        self.dropOffCars(data)
         #snag cars coming into intersec
-        #have some way to draw the cars...
+        #have some way to draw the cars... pretty similar to the road:
+            #1) move cars
+            #2) accelerate cars
+            #3) once cars get out of intersection, send them to next intersection
+            
             
 ##view
      #draws cars in the intersection   
--- sideRoads.py
+++ sideRoads.py
@@ -1,8 +1,34 @@
 from roads import *
+from simulation import *
 class sideRoads (Road):
-    def __init__ (self, )
-    #essentially same as a normal road but creates cars on the edge
+    def __init__ (self, data, dir, xN, yN, xP, yP,\
+                    carsListN, carsListP, speedLimit, secsBtCars):
+        super.__init__ (data, dir, xN, yN, xP, yP,\
+                    carsListN, carsListP, speedLimit)
+        self.secsBtCars = secsBtCars
+    def addCarsPeriodically (self, data):
+        if self.dir == [0,1]:
+            if self.yN == 0:
+                if timerIsNSecs (secsBtCars):
+                    self.carInN()
+            if self.yP == data.height:
+                if timerIsNSecs (secsBtCars):
+                    self.carInP()
+        if self.dir == [1,0]:
+            if self.xN == 0:
+                if timerIsNSecs (secsBtCars):
+                    self.carInN()
+            if self.xP == data.width:
+                if timerIsNSecs (secsBtCars):
+                    self.carInP()  
+                    
+    def timerFiredRoad (self, data):
+        super.timerFiredRoad (data)
+        self.addCarsPeriodically (data)
+                    
+                    
+                      #essentially same as a normal road but creates cars on the edge
     
     
     ## prob let's do side roads cuz dis same as de roads
-    
+    ##need to check cuz you're making road elements be like [road, N] and then you don't do anything about the indexing
