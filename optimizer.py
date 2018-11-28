d463c83772bb7b2961c1a9d9e8fcaf7fb9b89736
Carolyn Lee <cnl2@andrew.cmu.edu>
2018-11-28 17:56:21 -0500

    slick slick pls create better interface, check the errors, make cars go faster, why sometimes the three way not work for making cars turn

--- optimizer.py
+++ optimizer.py
@@ -27,30 +27,31 @@ def createTimes ():
         lights [i][0] = random.choice ([3, 4, 5, 6])
         lights [i][1] = random.choice ([3, 4, 5, 6])
 createTimes ()
-###################################roads list is a copy and the intersection also has roads, but they aren't the same roads list -- here we WANT some kind of same road list in the intersection as in the roads -- mebbe make a copy of roads and then create intersections around them????? USE HASHING to find out if they are the same
+
 ##run
 
 #look in simulator and see why nothing is drawing like cars or lights
-print (run (set = False, width = 800, height = 800, lights = lights, roads = roads, intersecs = intersecs))
-print (9)
-
-print (000000)
-print (run (set = False, width = 800, height = 800, lights = lights, roads = copyRoads (r), intersecs = copyIntersecs (i, roads)))
-# def optimize (runs):
-#     minAvg = 10**99
-#     for r in range (runs):
-#         createTimes()
-#         print (10)
-#         tmpAvg = (run (set = False, width = 800, height = 800, lights = lights, roads = roads, intersecs = intersecs)) [2]
-#         print (9)
-#         if minAvg > tmpAvg:
-#             print (8)
-#             minAvg = tmpAvg
-#             minLights = lights
-#         print (7)
-#     print (6)
-#     print ("ok look here are your nice nice lights")
-#     print (minAvg)
-#     
-#optimize (3)
+# print (run (set = False, width = 800, height = 800, lights = lights, roads = roads, intersecs = intersecs))
+# print (9)
+# 
+# print (000000)
+# roads = copyRoads (r)
+# intersecs = copyIntersecs (i, roads)
+# print (run (set = False, width = 800, height = 800, lights = lights, roads = roads, intersecs = intersecs))
+def optimize (runs, r, i):
+    minAvg = 10**99
+    for y in range (runs):
+        createTimes()
+        roads = copyRoads (r)
+        intersecs = copyIntersecs (i, roads)
+        tmpAvg = (run (set = False, width = 800, height = 800, lights = lights, roads = roads, intersecs = intersecs)) [2]
+        if tmpAvg == None:
+            print ("you need to let the thing run for a little bit so you get a bit of data" + 42/0)
+        if minAvg > tmpAvg:
+            minAvg = tmpAvg
+            minLights = lights
+    print ("ok look here are your nice nice lights" + str(minLights))
+    print ("here is the minimum avg" + minAvg)
+    
+optimize (3, r, i)
         
